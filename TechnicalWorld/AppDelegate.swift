8501270c33ed73d27c5c586f77f6ffc3a83f8b91
Rohit <rohit.dhakad@systematixindia.com>
2021-04-11 01:40:11 +0530

    first commit

---
+++ TechnicalWorld/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TechnicalWorld
+//
+//  Created by Paras on 10/04/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ TechnicalWorld/Classes/LoginSignup/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  TechnicalWorld
+//
+//  Created by Paras on 10/04/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
